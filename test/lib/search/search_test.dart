aea48ab83e202f5326cf4e5d7b29eab37d71f240
Andrés F. Rodriguez <82066965+Slorq-code@users.noreply.github.com>
2023-04-28 20:27:32 -0500

    update: add othe case of test

--- test/lib/search/search_test.dart
+++ test/lib/search/search_test.dart
@@ -8,9 +8,12 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   test('getCats() should populate cats list with Cats', () async {
     String testValue = "Aby";
+    String testValue2 = "curl";
     final service = BreedsService();
     final breedsResponse = await service.getBreedsByName(testValue);
+    final breedsResponse2 = await service.getBreedsByName(testValue2);
     expect(breedsResponse[0].name, equals("Abyssinian"));
+    expect(breedsResponse2[0].name, equals("American Curl"));
   });
 }
 
